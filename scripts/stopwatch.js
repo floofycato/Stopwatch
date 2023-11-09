2532a03bc631578034573bb5f31bd1554b941501
treshia <117747401+floofycato@users.noreply.github.com>
2023-11-09 17:07:22 +0800

    Added pause start reset functions

---
+++ scripts/stopwatch.js
@@ -0,0 +1,46 @@
+let [seconds, minutes, hours] = [0,0,0];
+let displayTime = document.getElementById("displayTime");
+let timer = null;
+
+function stopwatch() {
+  seconds++;
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes === 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+  
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+
+  displayTime.innerHTML = h + ':' + m + ':' + s
+}
+
+function watchStart() {
+  if (timer !== null) {
+    clearInterval(timer);
+  } 
+  timer = setInterval(stopwatch, 1000);
+  document.body.classList.add('active');
+  document.body.classList.remove('paused');
+}
+
+function watchStop() {
+  if (timer) {
+    clearInterval(timer);
+    document.body.classList.add('paused');
+    document.body.classList.remove('active');
+  }
+}
+
+function watchReset(){
+  clearInterval(timer);
+  [seconds, minutes, hours] = [0,0,0];
+  displayTime.innerHTML = '00:00:00';
+  document.body.classList.remove('active', 'paused');
+}
+
